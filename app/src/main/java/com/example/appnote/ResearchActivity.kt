4c78829ce9118d469791a9d423e9556b6f93f65a
Júlia Santos <julia.moura@compasso.com.br>
2020-06-02 10:24:06 -0300

    Allow research by description class researchActivity refactored

--- app/src/main/java/com/example/appnote/ResearchActivity.kt
+++ app/src/main/java/com/example/appnote/ResearchActivity.kt
@@ -9,13 +9,12 @@ import android.widget.ArrayAdapter
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_research.*
-import java.lang.Exception
 
 class ResearchActivity : AppCompatActivity() {
 
-    private var listOfItems = arrayOf("id", "title")
+    private var listOfItems = arrayOf("title", "description")
     var type = ""
-    var idDialog = ""
+    private var idDialog = ""
 
     private lateinit var firebaseFirestore: FirebaseFirestore
 
@@ -23,14 +22,24 @@ class ResearchActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_research)
 
-
         firebaseFirestore = FirebaseFirestore.getInstance()
 
+        configSpinner()
+
+        btn_search.setOnClickListener { makeResearch() }
+
+        cardViewResearch.setOnClickListener {goToAddActivity()}
+    }
+
+    private fun configSpinner(){
         spinner!!.onItemSelectedListener
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, listOfItems)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
+        spinnerAction()
+    }
 
+    private fun spinnerAction(){
         spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
@@ -43,36 +52,23 @@ class ResearchActivity : AppCompatActivity() {
             }
 
         }
-
-        btn_search.setOnClickListener { defineResearchType() }
-
-        cardViewResearch.setOnClickListener {
-            val intent = Intent(this, AddNotesActivity::class.java)
-            intent.putExtra("id", idDialog)
-            startActivity(intent)
-        }
     }
 
-
-    private fun defineResearchType(){
+    private fun makeResearch(){
         cardViewResearch.visibility = View.GONE
 
-        if(type == "title"){
-            makeResearchTitle()
-            return
-        }
-
-        makeResearchId()
-    }
-    private fun makeResearchTitle(){
-        firebaseFirestore.collection("notes")
-            .whereEqualTo("title", edtNote.text.toString())
+        firebaseFirestore.collection("notes").whereEqualTo(type, edtNote.text.toString())
             .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
             if(firebaseFirestoreException != null){
+                showToast()
                 return@addSnapshotListener
             }
+                if (querySnapshot == null || querySnapshot.isEmpty){
+                    showToast()
+                    return@addSnapshotListener
+                }
 
-            for (doc in querySnapshot!!){
+            for (doc in querySnapshot){
                 cardViewResearch.visibility = View.VISIBLE
                 idDialog = doc.id
                 doc.getString("title")?.let {
@@ -88,39 +84,13 @@ class ResearchActivity : AppCompatActivity() {
         }
     }
 
-    private fun makeResearchId(){
-
-        var note = 1
-
-        try {
-            note = edtNote.text.toString().toInt()
-        }catch (e: Exception){
-            showToast()
-        }
-
-        firebaseFirestore.collection("notes")
-            .whereEqualTo("id", note)
-            .addSnapshotListener { querySnapshot, firebaseFirestoreException ->
-                if(firebaseFirestoreException != null){
-                    return@addSnapshotListener
-                }
-
-                for (doc in querySnapshot!!){
-                    cardViewResearch.visibility = View.VISIBLE
-                    doc.getString("title")?.let {
-                        titleResearchTV.text = it
-                    }
-                    doc.getString("description")?.let {
-                        descResearchTV.text = it
-                    }
-                    doc.get("id")?.let {
-                        idResearchTV.text = it.toString()
-                    }
-                }
-            }
+    private fun showToast(){
+        Toast.makeText(this, "This item does not exists or is invalid, please try again", Toast.LENGTH_SHORT).show()
     }
 
-    private fun showToast(){
-        Toast.makeText(this, "Esse item é inválido ou não existe\nPor favor tente novamente", Toast.LENGTH_SHORT).show()
+    private fun goToAddActivity(){
+        val intent = Intent(this, AddNotesActivity::class.java)
+        intent.putExtra("id", idDialog)
+        startActivity(intent)
     }
 }
